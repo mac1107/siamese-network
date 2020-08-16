09c11f41898fe69fa7dfda5d7ef1a13123e91e0b
mac1107 <xucongma@163.com>
2020-08-16 17:07:00 +0800

    用变压器数据

--- siamese.py
+++ siamese.py
@@ -6,15 +6,16 @@ import numpy as np
 def siamese_loss(out1, out2, y, Q=5):
     Q = tf.constant(Q, name="Q", dtype=tf.float32)
     E_w = tf.sqrt(tf.reduce_sum(tf.square(out1 - out2), 1))
-    pos = tf.multiply(tf.multiply(y, 2 / Q), tf.square(E_w))
-    neg = tf.multiply(tf.multiply(1 - y, 2 * Q), tf.exp(-2.77 / Q * E_w))
+    pos = tf.multiply(tf.multiply(1 - y, 2 / Q), tf.square(E_w))
+    neg = tf.multiply(tf.multiply(y, 2 * Q), tf.exp(-2.77 / Q * E_w))
     loss = pos + neg
-    loss = tf.reduce_mean(loss)
+    loss = tf.reduce_mean(loss, name="loss")
     return loss
 
 
-def siamese(inputs, keep_prob):
+def siamese(inputs, keep_prob, batch):
     # conv1+relu1
+    # batch = tf.constant(batch, name="batch", dtype=tf.int32)
     with tf.name_scope('conv1') as scope:
         a = tf.random_normal_initializer
         # w1 = tf.Variable(tf.initializers.TruncatedNormal(stddev=0.05)(shape=[3, 3, 1, 32]), name='w1')
@@ -44,9 +45,10 @@ def siamese(inputs, keep_prob):
 
     # full connect+relu
     with tf.name_scope('fc1') as scope:
-        x_flat = tf.reshape(relu3, shape=[-1, 4 * 4 * 128])
-        w_fc1 = tf.Variable(tf.truncated_normal(shape=[4 * 4 * 128, 1024], stddev=0.05, mean=0), name='w_fc1')
-        b_fc1 = tf.Variable(tf.zeros(1024), name='b_fc1')
+        x_flat = tf.reshape(relu3, shape=[-1, batch * 128])
+        print(batch,keep_prob)
+        w_fc1 = tf.Variable(tf.truncated_normal(shape=[batch * 128, 512], stddev=0.05, mean=0), name='w_fc1')
+        b_fc1 = tf.Variable(tf.zeros(512), name='b_fc1')
         fc1 = tf.add(tf.matmul(x_flat, w_fc1), b_fc1)
     with tf.name_scope('relu_fc1') as scope:
         relu_fc1 = tf.nn.relu(fc1, name='relu_fc1')
@@ -57,17 +59,17 @@ def siamese(inputs, keep_prob):
     with tf.name_scope('bn_fc1') as scope:
         bn_fc1 = tf.layers.batch_normalization(drop_1, name='bn_fc1')
     with tf.name_scope('fc2') as scope:
-        w_fc2 = tf.Variable(tf.truncated_normal(shape=[1024, 512], stddev=0.05, mean=0), name='w_fc2')
-        b_fc2 = tf.Variable(tf.zeros(512), name='b_fc2')
+        w_fc2 = tf.Variable(tf.truncated_normal(shape=[512, 2], stddev=0.05, mean=0), name='w_fc2')
+        b_fc2 = tf.Variable(tf.zeros(2), name='b_fc2')
         fc2 = tf.add(tf.matmul(bn_fc1, w_fc2), b_fc2)
-    with tf.name_scope('relu_fc2') as scope:
-        relu_fc2 = tf.nn.relu(fc2, name='relu_fc2')
-    with tf.name_scope('drop_2') as scope:
-        drop_2 = tf.nn.dropout(relu_fc2, keep_prob=keep_prob, name='drop_2')
-    with tf.name_scope('bn_fc2') as scope:
-        bn_fc2 = tf.layers.batch_normalization(drop_2, name='bn_fc2')
-    with tf.name_scope('fc3') as scope:
-        w_fc3 = tf.Variable(tf.truncated_normal(shape=[512, 2], stddev=0.05, mean=0), name='w_fc3')
-        b_fc3 = tf.Variable(tf.zeros(2), name='b_fc3')
-        fc3 = tf.add(tf.matmul(bn_fc2, w_fc3), b_fc3)
-    return fc3
+    # with tf.name_scope('relu_fc2') as scope:
+    #     relu_fc2 = tf.nn.relu(fc2, name='relu_fc2')
+    # with tf.name_scope('drop_2') as scope:
+    #     drop_2 = tf.nn.dropout(relu_fc2, keep_prob=keep_prob, name='drop_2')
+    # with tf.name_scope('bn_fc2') as scope:
+    #     bn_fc2 = tf.layers.batch_normalization(drop_2, name='bn_fc2')
+    # with tf.name_scope('fc3') as scope:
+    #     w_fc3 = tf.Variable(tf.truncated_normal(shape=[512, 2], stddev=0.05, mean=0), name='w_fc3')
+    #     b_fc3 = tf.Variable(tf.zeros(2), name='b_fc3')
+    #     fc3 = tf.add(tf.matmul(bn_fc2, w_fc3), b_fc3)
+    return fc2
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
 
 import numpy as np
 from siamese import *
@@ -7,7 +7,8 @@ from siamese import *
 # tf.enable_eager_execution()
 tf.reset_default_graph()
 
-mnist = input_data.read_data_sets('./data/mnist', one_hot=True)
+
+# mnist = input_data.read_data_sets('./data/mnist', one_hot=True)
 
 
 # print(mnist.validation.num_examples)
@@ -46,31 +47,44 @@ def print_dataset(data_set):
 
 train_data = ""
 train_label = ""
+test_data = ""
+test_label = ""
+labels = {}
 
 
 def init():
-    global train_data, train_label
-    train_data = np.loadtxt("/Users/bytedance/.keras/datasets/trainfuck.csv", dtype=np.float, delimiter=',',
-                            usecols=(i for i in range(1, 10)))
-    train_label = np.loadtxt("/Users/bytedance/.keras/datasets/trainfuck.csv", dtype=np.float, delimiter=',',
-                             usecols=0)
-    print(train_data)
+    global train_data, train_label, test_data, test_label, labels
+    train_data = np.loadtxt("./data/train.csv", dtype=np.float, delimiter=',',
+                            usecols=(i for i in range(1, 26)), skiprows=1)
+    train_label = np.loadtxt("./data/train.csv", dtype=np.float, delimiter=',',
+                             usecols=0, skiprows=1)
+    for idx, label in enumerate(train_label):
+        if label not in labels:
+            labels[label] = [idx]
+        else:
+            labels[label].append(idx)
+    test_data = np.loadtxt("C:/Users/xucon/Desktop/eval.csv", dtype=np.float, delimiter=',',
+                           usecols=(i for i in range(1, 10)), skiprows=1)
+    test_label = np.loadtxt("C:/Users/xucon/Desktop/eval.csv", dtype=np.float, delimiter=',',
+                            usecols=0, skiprows=1)
+    print("test data:{}, test label:{}".format(test_data, test_label))
 
 
-def get_batch(batch_size):
-    idx = np.random.randint(0, len(train_data), size=batch_size)
+def get_batch(dataset, labelset, batch_size):
+    idx = np.random.randint(0, len(dataset), size=batch_size)
     data_batch = []
     label_batch = []
     for i in idx:
-        data_batch.append(train_data[i])
-        label_batch.append(train_label[i])
+        data_batch.append(dataset[i])
+        label_batch.append(labelset[i])
     return np.array(data_batch, dtype=float), np.array(label_batch, dtype=float)
 
 
 def run():
     lr = 0.01
     iterations = 20000
-    batch_size = 16
+    batch = 4
+    test_batch = 4
 
     # 占位符
     with tf.variable_scope('input_x1') as scope:
@@ -78,56 +92,79 @@ def run():
         # x_input_1 = tf.reshape(x1, [-1, 28, 28, 1])
         # x1 = tf.placeholder(tf.float32, shape=[None, 784])
         # x_input_1 = tf.reshape(x1, [-1, 28, 28, 1])
-        x1 = tf.placeholder(tf.float32, shape=[None, 9])
-        x_input_1 = tf.reshape(x1, [-1, 3, 3, 1])
+        x1 = tf.placeholder(tf.float32, shape=[None, 25])
+        x_input_1 = tf.reshape(x1, [-1, 5, 5, 1])
     with tf.variable_scope('input_x2') as scope:
         # x2 = tf.placeholder(tf.float32, shape=[None, 784])
         # x_input_2 = tf.reshape(x2, [-1, 28, 28, 1])
         # x2 = tf.placeholder(tf.float32, shape=[None, 784])
         # x_input_2 = tf.reshape(x1, [-1, 28, 28, 1])
-        x2 = tf.placeholder(tf.float32, shape=[None, 9])
-        x_input_2 = tf.reshape(x1, [-1, 3, 3, 1])
+        x2 = tf.placeholder(tf.float32, shape=[None, 25])
+        x_input_2 = tf.reshape(x1, [-1, 5, 5, 1])
     with tf.variable_scope('y') as scope:
-        y = tf.placeholder(tf.float32, shape=[batch_size])
+        y = tf.placeholder(tf.float32, shape=[batch])
 
     with tf.name_scope('keep_prob') as scope:
         keep_prob = tf.placeholder(tf.float32)
 
+    with tf.name_scope('batch_size') as scope:
+        batch_size = tf.placeholder(tf.int32)
+
     with tf.variable_scope('siamese') as scope:
-        out1 = siamese(x_input_1, keep_prob)
+        out1 = siamese(x_input_1, keep_prob, batch)
         scope.reuse_variables()
-        out2 = siamese(x_input_2, keep_prob)
+        out2 = siamese(x_input_2, keep_prob, batch)
     with tf.variable_scope('metrics') as scope:
         loss = siamese_loss(out1, out2, y)
         optimizer = tf.train.AdamOptimizer(lr).minimize(loss)
 
     loss_summary = tf.summary.scalar('loss', loss)
     merged_summary = tf.summary.merge_all()
-    # iterator = raw_train_data.make_one_shot_iterator()
+
     with tf.Session() as sess:
         writer = tf.summary.FileWriter('./graph/siamese', sess.graph)
         sess.run(tf.global_variables_initializer())
-        # sess.run(iterator.initializer, feed_dict={batch_size: 20})
         for itera in range(iterations):
-            xs_1, ys_1 = get_batch(16)
-            # xs_1, ys_1 = mnist.train.next_batch(batch_size)
-            # print(type(xs_1), xs_1, len(xs_1))
-            # print(type(ys_1), ys_1, len(ys_1))
-            # xs_1,ys_1=raw_train_data.batch(2)
-            # ys_1 = np.argmax(ys_1, axis=1)
-            xs_2, ys_2 = get_batch(16)
-            # xs_2, ys_2 = mnist.train.next_batch(batch_size)
-            # ys_2 = np.argmax(ys_2, axis=1)
-            # print(type(ys_1), ys_2)
-            y_s = np.array(ys_1 == ys_2, dtype=np.float32)
+            xs_1, ys_1 = get_batch(train_data, train_label, batch)
+
+            xs_2, ys_2 = get_batch(train_data, train_label, batch)
+
+            y_s = np.array(ys_1 != ys_2, dtype=np.float32)
             _, train_loss, summ = sess.run([optimizer, loss, merged_summary],
-                                           feed_dict={x1: xs_1, x2: xs_2, y: y_s, keep_prob: 0.6})
+                                           feed_dict={x1: xs_1, x2: xs_2, y: y_s, keep_prob: 0.6,
+                                                      batch_size: batch})
 
             writer.add_summary(summ, itera)
             if itera % 1000 == 1:
                 print('iter {},train loss {}'.format(itera, train_loss))
-        # embed = sess.run(out1, feed_dict={x1: mnist.test.images, keep_prob: 0.6})
-        # test_img = mnist.test.images.reshape([-1, 3, 3, 1])
+
+        # validate
+        # correct_count = 0
+        # wrong_count = 0
+        # for i in range(len(test_data)):
+        #     label_pre = 0
+        #
+        #     loss_pre = float('inf')
+        #     testdata = [test_data[i]] * batch
+        #     testlabel = [test_label[i]] * batch
+        #     ys = np.array([0] * batch, dtype=float)
+        #     for label in labels:
+        #         idx = np.random.randint(0, len(labels[label]), size=batch)
+        #         base_data = []
+        #         base_label = []
+        #         for j in idx:
+        #             base_data.append(train_data[j])
+        #             base_label.append(train_label[j])
+        #         result = sess.run(loss, feed_dict={x1: base_data, x2: testdata, y: ys, keep_prob: 0.6})
+        #         if result < loss_pre:
+        #             label_pre = label
+        #             loss_pre = result
+        #     if label_pre == testlabel[0]:
+        #         correct_count += 1
+        #     else:
+        #         wrong_count += 1
+        #
+        # print("corrent:{}, wrong:{}".format(correct_count, wrong_count))
         writer.close()
 
 
@@ -139,6 +176,4 @@ run()
 def test():
     a = [1, 2, 3, 4]
     print(np.array(a), type(np.array(a)))
-
-
-test()
+# test()
